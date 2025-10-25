70cd54e9b15c4a263cb36826dc3f27b738d5eb4c
Andrei-Olimpiu Balcu <abalcu@student.42heilbronn.de>
2025-10-25 06:07:19 +0200

    update

---
+++ Cursus/get_next_line/get_next_line_bonus.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abalcu <abalcu@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/21 12:53:42 by abalcu            #+#    #+#             */
+/*   Updated: 2025/10/25 01:16:29 by abalcu           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+# define MAX_FD 1024
+# include <stdint.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+size_t	ft_strlen(const char *s);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(const char *s);
+char	*ft_strchr(const char *s, int c);
+
+char	*get_next_line(int fd);
+
+#endif
