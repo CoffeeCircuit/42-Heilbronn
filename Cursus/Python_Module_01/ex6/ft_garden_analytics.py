2ea59cd2d7254851b0cc2db07ad27dc7923639d4
Andrei-Olimpiu Balcu <abalcu@student.42heilbronn.de>
2025-12-11 06:21:55 +0100

    update

--- Cursus/Python_Module_01/ex6/ft_garden_analytics.py
+++ Cursus/Python_Module_01/ex6/ft_garden_analytics.py
@@ -3,27 +3,48 @@
 
 class GardenStats:
 
-    def __init__(self, name: str) -> None:
-        self.name = name
+    def __init__(self) -> None:
+        self.name: str | None = None
         self.regular: int
         self.flowering: int
         self.prize: int
 
 
 class GardenManager:
-    max_gardens = 4
+    max_tokens: int = 4
+    used_tokens: int = 0
+    session: int | None = None
 
     def __init__(self) -> None:
-        self._gardens: list[GardenStats | None] = [None, None, None, None]
+        self._gardens: list[GardenStats] = [
+            GardenStats(),
+            GardenStats(),
+            GardenStats(),
+            GardenStats(),
+        ]
+
+    @staticmethod
+    def log(message: str, is_error: bool = False) -> None:
+        """Handler for loging messages"""
+        status: str = "Info"
+        if is_error == 1:
+            status = "Error"
+        print(f"G:> [ {status:^5} ] {message}")
 
     def add_garden(self, name: str):
         i = 0
-        while i < self.max_gardens:
-            if self._gardens[i] is None:
-                self._gardens[i] = GardenStats(name)
-
-    def log(self, name):
-        pass
+        while i < self.max_tokens:
+            if self._gardens[i].name is None:
+                self.log(f"'{name}' garden added.")
+                self._gardens[i].name = name
+                return
+            if self._gardens[i].name == name:
+                self.log("Name taken.", is_error=True)
+                exit()
+            i += 1
+        if i == 4:
+            self.log("Max number of gardens exceeded", is_error=True)
+            exit()
 
 
 class Plant:
@@ -55,7 +76,16 @@ class PrizeFlower(FloweringPlant):
 
 
 def main():
+    print("=== Garden Management System [GMS] ===")
     manager = GardenManager()
+    active_seesion = True
+    while (active_seesion):
+        kb_read = input("G:> ")
+        if kb_read == "exit":
+            manager.log("Exiting")
+            active_seesion = False
+
+
 
 
 if __name__ == "__main__":
