adf016b211dc1da18c5ca200b6a8105af9cc3b21
Andrei-Olimpiu Balcu <abalcu@student.42heilbronn.de>
2025-12-12 06:52:34 +0100

    ex5

--- Cursus/Python_Module_01/ex1/ft_garden_data.py
+++ Cursus/Python_Module_01/ex1/ft_garden_data.py
@@ -4,34 +4,26 @@
 class Plant:
     """Plant class"""
 
-    def __init__(self):
-        self._name: str
-        self._height: int
-        self._age: int
-
-    def add(self, name: str, height: int, age: int):
+    def __init__(self, name: str, height: int, age: int):
         self._name = name
         self._height = height
         self._age = age
 
-
-def plant_info(plant: Plant):
-    """Handler for printing plant information"""
-    print(f"{plant._name}: {plant._height}cm, {plant._age} days old")
+    def get_info(self):
+        """Handler for printing plant information"""
+        print(f"{self._name}: {self._height}cm, {self._age} days old")
 
 
 def main():
     """Main"""
-    plant1 = Plant()
-    plant2 = Plant()
-    plant3 = Plant()
-    plant1.add(name="Rose", height=25, age=30)
-    plant2.add(name="Sunflower", height=80, age=45)
-    plant3.add(name="Cactus", height=15, age=120)
+    plants: dict[str, Plant] = {}
+    plants["Rose"] = Plant("Rose", 25, 30)
+    plants["Sunflower"] = Plant("Sunflower", 80, 45)
+    plants["Cactus"] = Plant("Cactus", 15, 120)
+
     print("=== Garden Plant Registry ===")
-    plant_info(plant1)
-    plant_info(plant2)
-    plant_info(plant3)
+    for key in plants:
+        plants[key].get_info()
 
 
 if __name__ == "__main__":
--- Cursus/Python_Module_01/ex5/ft_plant_types.py
+++ Cursus/Python_Module_01/ex5/ft_plant_types.py
@@ -4,50 +4,71 @@
 class Plant:
     """Plant class"""
 
-    def __init__(self):
-        self._name: str
-        self._height: int
-        self._age: int
-
-    def add(self, name: str, height: int, age: int):
+    def __init__(self, name, height, age):
         self._name = name
         self._height = height
         self._age = age
+
+    def get_info(self):
+        """Handler for printing plant information"""
+        print(f"{self._name}: {self._height}cm, {self._age} days old")
 
 
 class Flower(Plant):
     """Flower class"""
 
-    def __init__(self):
-        super().__init__()
-        self.color: str
+    def __init__(self, name, height, age, color):
+        super().__init__(name, height, age)
+        self._color = color
 
     def bloom(self):
         pass
+
+    def get_info(self):
+        print(f"{self._name} (Flower): {self._height}cm, ", end="")
+        print(f"{self._age} days, {self._color} color")
 
 
 class Tree(Plant):
     """Tree class"""
 
-    def __init__(self):
-        super().__init__()
-        self.trunk_diameter: str
+    def __init__(self, name, height, age, trunk_diameter: int):
+        super().__init__(name, height, age)
+        self._trunk_diameter = trunk_diameter
 
     def produce_shade(self):
         pass
+
+    def get_info(self):
+        print(f"{self._name} (Tree): {self._height}cm, ", end="")
+        print(f"{self._age} days, {self._trunk_diameter}cm diameter")
 
 
 class Vegetable(Plant):
     """Vegetable class"""
 
-    def __init__(self):
-        super().__init__()
-        self.harvest_season: str
-        self.nutritional_value: str
+    def __init__(self, name, height, age, harvest_season, nutritional_value):
+        super().__init__(name, height, age)
+        self._harvest_season = harvest_season
+        self._nutritional_value = nutritional_value
+
+    def get_info(self):
+        print(f"{self._name} (Vegetable): {self._height}cm, ", end="")
+        print(f"{self._age} days, {self._harvest_season}")
+        print(f"{self._name} is {self._nutritional_value}")
 
 
 def main():
-    pass
+    print("=== Garden Plant Types ===")
+
+    plants: dict[str, Plant | Flower | Tree | Vegetable] = {
+        "rose": Flower("Rose", 25, 30, "red"),
+        "oak": Tree("Oak", 500, 1825, 50),
+        "tomato": Vegetable("Tomato", 80, 90, "summer", "rich in vitamin C"),
+    }
+
+    for key in plants:
+        plants[key].get_info()
 
 
 if __name__ == "__main__":
