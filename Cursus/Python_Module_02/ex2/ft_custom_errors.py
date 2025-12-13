33da499f92a6d22ccbffbb298a7703814a849b08
Andrei-Olimpiu Balcu <abalcu@student.42heilbronn.de>
2025-12-13 09:33:50 +0100

    added logic

--- Cursus/Python_Module_02/ex2/ft_custom_errors.py
+++ Cursus/Python_Module_02/ex2/ft_custom_errors.py
@@ -7,17 +7,44 @@ class GardenError(Exception):
 
 
 class PlantError(GardenError):
-    def __init__(self, *args: object) -> None:
-        super().__init__(*args)
+    def __init__(self, *args: object, health) -> None:
+        if health < 2:
+            super().__init__("Health error")
+        else:
+            super().__init__(*args)
+        self._health = health
 
 
 class WaterError(GardenError):
-    def __init__(self, *args: object) -> None:
+    def __init__(self, *args: object, water) -> None:
         super().__init__(*args)
+        self.water = water
 
 
 def main():
-    pass
+    print("=== Custom Garden Errors ===")
+
+    print()
+    print("Testing GardenError")
+    try:
+        raise GardenError("From GardenError")
+    except GardenError as e:
+        print("Error: ", e)
+
+    print()
+    print("Testing PlantError")
+    try:
+        raise PlantError("Initial PlantError", health=1)
+    except PlantError as e:
+        print("Error: ", e)
+
+    print()
+    print("Testing WaterError")
+    try:
+        raise WaterError("From WaterError", water=-5)
+    except WaterError as e:
+        print("Error: ", e)
+        print("\tWather level: ", e.water)
 
 
 if __name__ == "__main__":
