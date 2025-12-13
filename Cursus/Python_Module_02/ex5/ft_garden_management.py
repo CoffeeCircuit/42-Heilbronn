bd096de7f0caa130c445732d5728be7497e2929e
Andrei-Olimpiu Balcu <abalcu@student.42heilbronn.de>
2025-12-13 07:42:24 +0100

    Added more exception handling

--- Cursus/Python_Module_02/ex5/ft_garden_management.py
+++ Cursus/Python_Module_02/ex5/ft_garden_management.py
@@ -2,29 +2,60 @@
 
 
 class PlantException(Exception):
-    def __init__(self, *args: object, name: str | None) -> None:
-        super().__init__(*args)
+    def __init__(self, message: str, name: str | None = None) -> None:
+        super().__init__(message)
         self.name = name
 
 
 class Plant:
 
     def __init__(self, name: str | None, water: int, health: int) -> None:
-        self._name = name
-        self._water = water
-        self._health = health
+        self.name = name
+        self.water = water
+        self.health = health
 
     @property
     def name(self):
-        if self._name is None:
-            raise PlantException("Plant name cannot be None", name=self._name)
+        if self._name is None or self._name == "":
+            msg = f"Plant '{self._name}': Invalid name"
+            raise PlantException(msg, name=self._name)
         return self._name
 
     @name.setter
-    def name(self, name: str):
-        if name is None:
-            raise PlantException("Plant name cannot be None", name=self._name)
+    def name(self, name: str | None):
+        if name is None or name == "":
+            msg = f"Plant '{name}': Invalid name"
+            raise PlantException(msg, name=name)
         self._name = name
+
+    @property
+    def water(self):
+        if self._water < 0:
+            msg = f"Plant '{self._name}': Negative water value '{self._water}'"
+            raise PlantException(msg, name=self._name)
+        return self._water
+
+    @water.setter
+    def water(self, water: int):
+        if water < 0:
+            msg = f"Plant '{self._name}': Negative water value '{water}'"
+            raise PlantException(msg, name=self._name)
+        self._water = water
+
+    @property
+    def health(self):
+        if self._health < 0:
+            _health = self._health
+            msg = f"Plant '{self._name}': Negative health value '{_health}'"
+            raise PlantException(msg, name=self._name)
+        return self._health
+
+    @health.setter
+    def health(self, health: int):
+        if health < 0:
+            msg = f"Plant '{self._name}': Negative health value '{health}'"
+            raise PlantException(msg, name=self._name)
+        self._health = health
 
 
 class GardenManager:
@@ -32,24 +63,27 @@ class GardenManager:
     def __init__(self) -> None:
         self.plants: dict[str, Plant] = {}
 
-    def add_plant(self, plant: Plant):
+    def add_plant(self, name: str | None, water: int, health: int):
         try:
+            plant = Plant(name, water, health)
             self.plants[plant.name] = plant
-            print(f"Pland {plant.name} added")
+            print(f"Plant '{plant.name}' added successfully")
         except PlantException as e:
             print("Error:", e)
 
-    def water_plans(self):
-        pass
-
-    def check_health(self):
-        pass
-
 
 def main():
+    print("=== Garden Management System ===")
     manager = GardenManager()
-    manager.add_plant(Plant("Rose", 5, 10))
-    manager.add_plant(Plant(None, 5, 10))
+    manager.add_plant("Rose", 5, 10)
+    manager.add_plant("Tomato", 10, 12)
+    manager.add_plant("Lettuce", 1, 2)
+
+    # error testing
+    manager.add_plant(None, 5, 10)
+    manager.add_plant("", 5, 10)
+    manager.add_plant("Foo", -5, 10)
+    manager.add_plant("Bar", 5, -10)
 
 
 if __name__ == "__main__":
