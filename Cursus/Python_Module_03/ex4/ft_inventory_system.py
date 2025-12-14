dbba2309f65378fa036a8486e5a267569a58f528
Andrei-Olimpiu Balcu <abalcu@student.42heilbronn.de>
2025-12-14 09:39:20 +0100

    update

--- Cursus/Python_Module_03/ex4/ft_inventory_system.py
+++ Cursus/Python_Module_03/ex4/ft_inventory_system.py
@@ -7,7 +7,7 @@ update()
 """
 
 
-def print_inventory(player: str, db: dict[str, dict[str, dict]]):
+def print_inventory(db: dict[str, dict[str, dict]], player: str):
     print(f"=== {player}'s Inventory ===")
     inventory = db[player]
     inventory_value = 0
@@ -29,18 +29,23 @@ def print_inventory(player: str, db: dict[str, dict[str, dict]]):
     print(f"Inventory value: {inventory_value} gold")
     print(f"Item count: {item_count} items")
     print("Categories: ", end="")
+    i = 0
     for cat, count in categories.items():
-        print(f"{cat}({count})", end=", ")
+        i += 1
+        if i < len(categories):
+            print(f"{cat}({count})", end=", ")
+        else:
+            print(f"{cat}({count})")
     print()
 
 
-def add_player(player: str, db: dict):
+def add_player(db: dict, player: str):
     if player in db.keys():
         raise Exception("Existing player")
     db[player] = dict()
 
 
-def add_item(player: str, db: dict, item: str, stat: dict[str, int | str]):
+def add_item(db: dict, player: str, item: str, stat: dict[str, int | str]):
     if player not in db.keys():
         raise Exception("Player not found")
     db[player].update({item: stat})
@@ -55,6 +60,16 @@ def set_stat(qty: int, val: int, type: str, rare: str) -> dict[str, int | str]:
     }
 
 
+def send_item(db, from_player: str, to_player: str, item_type: str, qty: int):
+    print(f"=== Transaction: {from_player} gives {to_player}", end="")
+    print(f"{qty} {item_type} ===")
+    print("Transaction successful!")
+
+
+def print_analytics(db: dict):
+    print("=== Inventory Analytics ===")
+
+
 def ft_inventory_system():
     """Main function"""
     print("=== Player Inventory System ===")
@@ -62,20 +77,37 @@ def ft_inventory_system():
     db = dict()
 
     try:
-        add_player("Alice", db)
-        add_player("Bob", db)
-        add_item("Alice", db, "sword", set_stat(1, 500, "weapon", "rare"))
+        add_player(db, "Alice")
+        add_player(db, "Bob")
         add_item(
-            "Alice",
             db,
+            "Alice",
+            "sword",
+            set_stat(1, 500, "weapon", "rare"),
+        )
+        add_item(
+            db,
+            "Alice",
             "potion",
             set_stat(5, 50, "consumable", "common"),
         )
-        add_item("Alice", db, "shield", set_stat(1, 200, "armor", "uncommon"))
+        add_item(
+            db,
+            "Alice",
+            "shield",
+            set_stat(1, 200, "armor", "uncommon"),
+        )
     except Exception as e:
         print("Error:", e)
 
-    print_inventory("Alice", db)
+    print_inventory(db, "Alice")
+
+    try:
+        send_item(db, "Alice", "Bob", "potion", 2)
+    except Exception as e:
+        print("Error:", e)
+
+    print_analytics(db)
 
 
 if __name__ == "__main__":
