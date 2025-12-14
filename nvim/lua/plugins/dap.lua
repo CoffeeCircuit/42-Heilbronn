6c05a023317f5c9fddc5456a1dcde66b43f3f17a
Andrei-Olimpiu Balcu <abalcu@student.42heilbronn.de>
2025-12-14 07:49:45 +0100

    update

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -1,102 +1,80 @@
+
 return {
-	{
-		"mfussenegger/nvim-dap",
-		dependencies = {
-			"rcarriga/nvim-dap-ui",
-			"nvim-neotest/nvim-nio"
-		},
-		keys = {
-			-- Basic debugging keymaps, feel free to change to your liking!
-			{
-				'<F5>',
-				function()
-					require('dap').continue()
-				end,
-				desc = 'Debug: Start/Continue',
-			},
-			{
-				'<F11>',
-				function()
-					require('dap').step_into()
-				end,
-				desc = 'Debug: Step Into',
-			},
-			{
-				'<F10>',
-				function()
-					require('dap').step_over()
-				end,
-				desc = 'Debug: Step Over',
-			},
-			{
-				'<F6>',
-				function()
-					require('dap').step_out()
-				end,
-				desc = 'Debug: Step Out',
-			},
-			{
-				'<leader>b',
-				function()
-					require('dap').toggle_breakpoint()
-				end,
-				desc = 'Debug: Toggle Breakpoint',
-			},
-			{
-				'<leader>B',
-				function()
-					require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ')
-				end,
-				desc = 'Debug: Set Breakpoint',
-			},
-			-- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
-			{
-				'<F7>',
-				function()
-					require('dapui').toggle()
-				end,
-				desc = 'Debug: See last session result.',
-			},
-		},
-			config = function()
-				local dapui = require("dapui")
-				local dap = require("dap")
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "rcarriga/nvim-dap-ui",
+      "nvim-neotest/nvim-nio"
+    },
+    keys = {
+      { '<F5>',  function() require('dap').continue() end, desc = 'Debug: Start/Continue' },
+      { '<F11>', function() require('dap').step_into() end, desc = 'Debug: Step Into' },
+      { '<F10>', function() require('dap').step_over() end, desc = 'Debug: Step Over' },
+      { '<F6>',  function() require('dap').step_out() end, desc = 'Debug: Step Out' },
+      { '<leader>b', function() require('dap').toggle_breakpoint() end, desc = 'Debug: Toggle Breakpoint' },
+      { '<leader>B', function() require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ') end, desc = 'Debug: Set Breakpoint' },
+      { '<F7>',  function() require('dapui').toggle() end, desc = 'Debug: Toggle UI' },
+    },
+    config = function()
+      local dapui = require("dapui")
+      local dap = require("dap")
 
-				dapui.setup()
+      dapui.setup()
 
-				dap.adapters.codelldb = {
-					type = "server",
-					port = "${port}",
-					executable = {
-						command = vim.fn.expand(
-							"~/.local/share/nvim/mason/packages/codelldb/extension/adapter/codelldb"
-						),
-						args = { "--port", "${port}" },
-					},
-				}
+      -- Path to cpptools adapter installed by Mason
+      local mason_path = vim.fn.stdpath("data") .. "/mason/packages/cpptools/extension/debugAdapters/bin/OpenDebugAD7"
 
-				dap.configurations.cpp = {
-					{
-						name = "Launch file",
-						type = "codelldb",
-						request = "launch",
-						program = function()
-							local file = vim.fn.expand('%:r')  -- current file without extension
-							local bin = vim.fn.getcwd() .. "/" .. vim.fn.fnamemodify(file, ':t')
-							return vim.fn.input('Path to executable: ', bin, 'file')
-						end,
-						cwd = '${workspaceFolder}',
-						stopOnEntry = false,
-					},
-				}
+      dap.adapters.cppdbg = {
+        id = 'cppdbg',
+        type = 'executable',
+        command = mason_path,
+      }
 
-				dap.configurations.c = dap.configurations.cpp
-				dap.configurations.rust = dap.configurations.cpp
+dap.configurations.cpp = {
+  {
+    name = "Launch file",
+    type = "cppdbg",
+    request = "launch",
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    cwd = '${workspaceFolder}',
+    stopAtEntry = true,
+  },
+  {
+    name = 'Attach to gdbserver :1234',
+    type = 'cppdbg',
+    request = 'launch',
+    MIMode = 'gdb',
+    miDebuggerServerAddress = 'localhost:1234',
+    miDebuggerPath = '/usr/bin/gdb',
+    cwd = '${workspaceFolder}',
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+  },
+  {
+	  setupCommands = {  
+		  { 
+			  text = '-enable-pretty-printing',
+			  description =  'enable pretty printing',
+			  ignoreFailures = false 
+		  },
+	  },
+  },
 
-				dap.listeners.after.event_initialized['dapui_config'] = dapui.open
-				dap.listeners.before.event_terminated['dapui_config'] = dapui.close
-				dap.listeners.before.event_exited['dapui_config'] = dapui.close
+}
 
-			end
-		},
-	}
+      dap.configurations.c = dap.configurations.cpp
+      dap.configurations.rust = dap.configurations.cpp
+
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end
+  },
+}
