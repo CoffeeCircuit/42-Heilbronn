ae6a5c0cf76256ffca8a307de58bbb82525bd9c4
Andrei-Olimpiu Balcu <abalcu@student.42heilbronn.de>
2026-01-18 12:51:24 +0100

    update

--- Cursus/Python_Module_05/ex2/nexus_pipeline.py
+++ Cursus/Python_Module_05/ex2/nexus_pipeline.py
@@ -6,21 +6,11 @@ Authorized: isinstance(), print(), collections
 """
 
 from abc import ABC, abstractmethod
-from typing import Protocol
+from typing import Protocol, runtime_checkable
 from typing import Any, Union, Optional, List, Dict
 
 
-class ProcessingPipeline(ABC):
-    pass
-
-    @abstractmethod
-    def process(self, data: Any) -> Any:
-        pass
-
-    def add_stage(self):
-        pass
-
-
+@runtime_checkable
 class ProcessingStage(Protocol):
     """Interface for stages"""
 
@@ -47,10 +37,34 @@ class OutputStage:
         pass
 
 
-class SONAdapter(ProcessingPipeline):
+class ProcessingPipeline(ABC):
+    def __init__(self) -> None:
+        super().__init__()
+        self.stages: List[ProcessingStage] = []
+
+    @abstractmethod
+    def process(self, data: Any) -> Any:
+        pass
+
+    def add_stage(self, stage: ProcessingStage):
+        if not isinstance(stage, ProcessingStage):
+            raise TypeError("Stage must implement process()")
+        self.stages.append(stage)
+
+
+class JSONAdapter(ProcessingPipeline):
     def __init__(self, pipeline_id) -> None:
         super().__init__()
         self._id = pipeline_id
+
+    def process(self, data: Any) -> Any:
+        step = data
+        for stage in self.stages:
+            try:
+                step = stage.process(step)
+            except Exception as e:
+                print(f"Error in stage {stage.__class__.__name__}: {e}")
+        return step
 
 
 class CSVAdapter(ProcessingPipeline):
@@ -58,16 +72,45 @@ class CSVAdapter(ProcessingPipeline):
         super().__init__()
         self._id = pipeline_id
 
+    def process(self, data: Any) -> Any:
+        step = data
+        for stage in self.stages:
+            try:
+                step = stage.process(step)
+            except Exception as e:
+                print(f"Error in stage {stage.__class__.__name__}: {e}")
+        return step
+
 
 class StreamAdapter(ProcessingPipeline):
     def __init__(self, pipeline_id) -> None:
         super().__init__()
         self._id = pipeline_id
 
+    def process(self, data: Any) -> Any:
+        step = data
+        for stage in self.stages:
+            try:
+                step = stage.process(step)
+            except Exception as e:
+                print(f"Error in stage {stage.__class__.__name__}: {e}")
+        return step
+
 
 class NexusManager:
-    def add_pipeline():
-        pass
+    def __init__(self) -> None:
+        self.pipelines: List[ProcessingPipeline] = []
 
-    def process_data():
-        pass
+    def add_pipeline(self, pipeline: ProcessingPipeline):
+        if not isinstance(pipeline, ProcessingPipeline):
+            raise TypeError("Invalid pipeline")
+        self.pipelines.append(pipeline)
+
+    def process_data(self, data: Any) -> Any:
+        result = data
+        for pipeline in self.pipelines:
+            try:
+                result = pipeline.process(result)
+            except Exception as e:
+                print(f"Error processing {pipeline}: {e}")
+        return result
